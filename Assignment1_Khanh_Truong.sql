e9873ad95456be9c57c1bc1ac2de7cf45a66f356
khanhtruong342 <125117085+khanhtruong342@users.noreply.github.com>
2023-02-11 14:51:45 +0700

    Update and rename Assignment1.sql to Assignment1_Khanh_Truong.sql

--- Assignment1_Khanh_Truong.sql
+++ Assignment1_Khanh_Truong.sql
@@ -1,11 +1,11 @@
+DROP DATABASE IF EXISTS fresher_training_management;
 
-drop database if exists fresher_training_management;
+CREATE DATABASE IF NOT EXISTS fresher_training_management;
 
-create database if not exists fresher_training_management;
+USE fresher_training_management;
 
-use fresher_training_management;
 /* CREATE TABLE */
-create table trainee(
+CREATE TABLE trainee(
     TraineeID INT PRIMARY KEY auto_increment NOT NULL,
     Full_Name VARCHAR(50) NOT NULL,
     Birth_Date Date NOT NULL,
@@ -13,10 +13,10 @@ create table trainee(
     ET_IQ tinyint check(ET_IQ >= 0 AND ET_IQ <=20),
     ET_Gmath tinyint check(ET_Gmath >= 0 AND ET_Gmath <=20),
     ET_English tinyint check(ET_English >= 0 AND ET_English <=50),
-    Training_Class VARCHAR(50) NOT NULL,
+    Training_Class VARCHAR(100) NOT NULL,
     Evaluation_Notes VARCHAR(250)
 );
 
-/* ADD FIELD */
+-- ADD FIELD 
 ALTER TABLE Trainee
 ADD VTI_Account VARCHAR(50) NOT NULL UNIQUE;
