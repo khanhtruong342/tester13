9ecdcaf01138e869f0db2fdd0c251a187503c32a
khanhtruong342 <125117085+khanhtruong342@users.noreply.github.com>
2023-02-11 14:50:58 +0700

    TEST
    LESSON 2

---
+++ Assignment1.sql
@@ -0,0 +1,22 @@
+
+drop database if exists fresher_training_management;
+
+create database if not exists fresher_training_management;
+
+use fresher_training_management;
+/* CREATE TABLE */
+create table trainee(
+    TraineeID INT PRIMARY KEY auto_increment NOT NULL,
+    Full_Name VARCHAR(50) NOT NULL,
+    Birth_Date Date NOT NULL,
+    Gender ENUM('male', 'female', 'unknown'),
+    ET_IQ tinyint check(ET_IQ >= 0 AND ET_IQ <=20),
+    ET_Gmath tinyint check(ET_Gmath >= 0 AND ET_Gmath <=20),
+    ET_English tinyint check(ET_English >= 0 AND ET_English <=50),
+    Training_Class VARCHAR(50) NOT NULL,
+    Evaluation_Notes VARCHAR(250)
+);
+
+/* ADD FIELD */
+ALTER TABLE Trainee
+ADD VTI_Account VARCHAR(50) NOT NULL UNIQUE;
